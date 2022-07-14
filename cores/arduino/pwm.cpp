07e131de69b084f64771cb52619a6c4d2437637c
giulcioffi <g.cioffi@arduino.cc>
2022-07-14 15:07:31 +0200

    PwmOut class: add APIs to use us instead of ms only
    Former-commit-id: ac7876aebf6fd77a57f329efbf4cde1019ad8d18

--- cores/arduino/pwm.cpp
+++ cores/arduino/pwm.cpp
@@ -8,7 +8,9 @@ PwmOut::PwmOut(pin_size_t pinNumber) :
   _pin(pinNumber),
   _pwm_idx(0),
   _period(0),
+  _period_us(0),
   _pulse_width(0),
+  _pulse_width_us(0),
   _enabled(false)
 {
 
@@ -45,20 +47,39 @@ bool PwmOut:: begin(uint32_t period_width, uint32_t pulse_width) {
 
 bool PwmOut::period(int ms) {
   pclkd_freq_hz = R_FSP_SystemClockHzGet(FSP_PRIV_CLOCK_PCLKD) >> pwmTable[_pwm_idx].gpt_cfg->source_div;
-  uint32_t period_counts = (uint32_t) ((uint64_t) pclkd_freq_hz/1000 * ms);
-  if (R_GPT_PeriodSet(pwmTable[_pwm_idx].gpt_ctrl, period_counts) != FSP_SUCCESS) {
+  uint32_t period_counts_ms = (uint32_t) ((uint64_t) pclkd_freq_hz/1000 * ms);
+  if (R_GPT_PeriodSet(pwmTable[_pwm_idx].gpt_ctrl, period_counts_ms) != FSP_SUCCESS) {
     return false;
   }
-  _period = ms;
+  _period_ms = ms*1000;
   return true;
 }
 
 bool PwmOut::pulseWidth(int ms) {
-  uint32_t pulse_counts = (uint32_t) ((uint64_t) pclkd_freq_hz/1000 * ms);
-  if (R_GPT_DutyCycleSet(pwmTable[_pwm_idx].gpt_ctrl, pulse_counts, pwmTable[_pwm_idx].gpt_pin) != FSP_SUCCESS) {
+  uint32_t pulse_counts_ms = (uint32_t) ((uint64_t) pclkd_freq_hz/1000 * ms);
+  if (R_GPT_DutyCycleSet(pwmTable[_pwm_idx].gpt_ctrl, pulse_counts_ms, pwmTable[_pwm_idx].gpt_pin) != FSP_SUCCESS) {
     return false;
   }
-  _pulse_width = ms;
+  _pulse_width_ms = ms*1000;
+  return true;
+}
+
+bool PwmOut::period_us(int us) {
+  pclkd_freq_hz = R_FSP_SystemClockHzGet(FSP_PRIV_CLOCK_PCLKD) >> pwmTable[_pwm_idx].gpt_cfg->source_div;
+  uint32_t period_counts_us = (uint32_t) ((uint64_t) pclkd_freq_hz/1000000 * us);
+  if (R_GPT_PeriodSet(pwmTable[_pwm_idx].gpt_ctrl, period_counts_us) != FSP_SUCCESS) {
+    return false;
+  }
+  _period_us = us;
+  return true;
+}
+
+bool PwmOut::pulseWidth_us(int us) {
+  uint32_t pulse_counts_us = (uint32_t) ((uint64_t) pclkd_freq_hz/1000000 * us);
+  if (R_GPT_DutyCycleSet(pwmTable[_pwm_idx].gpt_ctrl, pulse_counts_us, pwmTable[_pwm_idx].gpt_pin) != FSP_SUCCESS) {
+    return false;
+  }
+  _pulse_width_us = us;
   return true;
 }
 
--- cores/arduino/pwm.h
+++ cores/arduino/pwm.h
@@ -12,6 +12,8 @@ class PwmOut {
     void end();
     bool period(int ms);
     bool pulseWidth(int ms);
+    bool period_us(int us);
+    bool pulseWidth_us(int us);
     uint32_t getPeriod();
     uint32_t getPulseWidth();
     void suspend();
@@ -22,7 +24,9 @@ class PwmOut {
     pin_size_t _pin;
     pin_size_t _pwm_idx;
     uint32_t _period;
+    uint32_t _period_us;
     uint32_t _pulse_width;
+    uint32_t _pulse_width_us;
 };
 
 #endif //__ARDUINO_PWM_H__
