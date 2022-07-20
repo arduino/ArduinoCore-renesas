f94a71c8e3d7a269ef65802a12b8cb2f6d715eb6
giulcioffi <g.cioffi@arduino.cc>
2022-07-20 15:15:34 +0200

    Simplify pinPeripheral() implementation
    Former-commit-id: 7d42dab364a2a383b5793f7155e2bbf8e83092e3

--- cores/arduino/pinDefinitions.cpp
+++ cores/arduino/pinDefinitions.cpp
@@ -7,32 +7,7 @@
 #define BSP_IO_PRV_8BIT_MASK              (0xFF)
 
 void pinPeripheral(bsp_io_port_pin_t bspPin, uint32_t bspPeripheral) {
-
-    bsp_io_port_pin_t pin;
-    ioport_cfg_t * p_pin_data = (ioport_cfg_t *) g_ioport.p_cfg;
-
-    R_BSP_PinAccessEnable();           // Protect PWPR from re-entrancy
-
-    g_APinDescription[bspPin].PeripheralConfig = bspPeripheral;
-    pin = g_APinDescription[bspPin].name;
-
-    /* PMR bits should be cleared before specifying PSEL. Reference section "20.7 Notes on the PmnPFS Register Setting"
-     * in the RA6M3 manual R01UH0886EJ0100. */
-    if ((bspPeripheral & IOPORT_PRV_PERIPHERAL_FUNCTION) > 0)
-    {
-        /* Clear PMR */
-        R_PFS->PORT[pin >> IOPORT_PRV_PORT_OFFSET].PIN[pin & BSP_IO_PRV_8BIT_MASK].PmnPFS_b.PMR = 0;
-
-        /* New config with PMR = 0 */
-        R_PFS->PORT[pin >> IOPORT_PRV_PORT_OFFSET].PIN[pin &
-                                                       BSP_IO_PRV_8BIT_MASK].PmnPFS =
-            (bspPeripheral & ~((uint32_t) IOPORT_PRV_PERIPHERAL_FUNCTION));
-    }
-
-    /* Write configuration */
-    R_PFS->PORT[pin >> IOPORT_PRV_PORT_OFFSET].PIN[pin & BSP_IO_PRV_8BIT_MASK].PmnPFS = bspPeripheral;
-
-    R_BSP_PinAccessDisable();
+    R_IOPORT_PinCfg(&g_ioport_ctrl, bspPin, bspPeripheral);
 }
 
 
