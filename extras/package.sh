71c669d5676e44f882809272d556bb420067ca8b
Martino Facchin <m.facchin@arduino.cc>
2022-08-01 14:51:56 +0200

    Fix package script to automatically create json
    Former-commit-id: 2cf095208e615b815f5fc41a993a41b8bbbbf8c9

--- extras/package.sh
+++ extras/package.sh
@@ -8,7 +8,24 @@ fi
 VERSION=`cat platform.txt | grep "version=" | cut -f2 -d"="`
 echo $VERSION
 
+FILENAME=ArduinoCore-renesas-$VERSION.tar.bz2
+
 CORE_BASE=`basename $PWD`
 cd ..
-tar --exclude='*.vscode*' --exclude='*.git*' --exclude='*e2studio*' --exclude='*extras*' -cjhvf ArduinoCore-renesas-$VERSION.tar.bz2 $CORE_BASE
+tar --exclude='*.vscode*' --exclude='*.git*' --exclude='*e2studio*' --exclude='*extras*' -cjhvf $FILENAME $CORE_BASE
 cd -
+
+mv ../$FILENAME .
+
+CHKSUM=`sha256sum $FILENAME | awk '{ print $1 }'`
+SIZE=`wc -c $FILENAME | awk '{ print $1 }'`
+
+cat extras/package_index.json.template |
+# sed "s/%%BUILD_NUMBER%%/${BUILD_NUMBER}/" |
+# sed "s/%%CURR_TIME%%/${CURR_TIME_SED}/" |
+sed "s/%%VERSION%%/${VERSION}/" |
+sed "s/%%FILENAME%%/${FILENAME}/" |
+sed "s/%%CHECKSUM%%/${CHKSUM}/" |
+sed "s/%%SIZE%%/${SIZE}/" > package_renesas_${VERSION}_index.json
+
+cat package_renesas_${VERSION}_index.json
