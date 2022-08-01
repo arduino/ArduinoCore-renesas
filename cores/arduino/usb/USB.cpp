fe44f3b7543eeca40986151ec478e7c71c234f61
Martino Facchin <m.facchin@arduino.cc>
2022-08-01 14:30:14 +0200

    USB: add proper support for High speed
    Former-commit-id: c9ccc91291acb37f51c4a2e85f3977a4de766244

--- cores/arduino/usb/USB.cpp
+++ cores/arduino/usb/USB.cpp
@@ -34,7 +34,11 @@
 #define USBD_CDC_EP_OUT (0x02)
 #define USBD_CDC_EP_IN (0x82)
 #define USBD_CDC_CMD_MAX_SIZE (8)
+#if (CFG_TUSB_RHPORT1_MODE & OPT_MODE_DEVICE)
+#define USBD_CDC_IN_OUT_MAX_SIZE (512)
+#else
 #define USBD_CDC_IN_OUT_MAX_SIZE (64)
+#endif
 
 #define USBD_STR_0 (0x00)
 #define USBD_STR_MANUF (0x01)
