a04aa19be6e7cc6dfba742913497ed440e888981
Martino Facchin <m.facchin@arduino.cc>
2022-09-22 14:26:14 +0200

    Serial: remove useless config redefinition
    Former-commit-id: c0c311d07dccada4c76bd8ae7391906253b1fde6

--- cores/arduino/Serial.h
+++ cores/arduino/Serial.h
@@ -51,57 +51,6 @@
 
 #define MAX_UARTS    10
 
-#undef SERIAL_5N1
-#undef SERIAL_6N1
-#undef SERIAL_7N1
-#undef SERIAL_8N1
-#undef SERIAL_5N2
-#undef SERIAL_6N2
-#undef SERIAL_7N2
-#undef SERIAL_8N2
-#undef SERIAL_5E1
-#undef SERIAL_6E1
-#undef SERIAL_7E1
-#undef SERIAL_8E1
-#undef SERIAL_5E2
-#undef SERIAL_6E2
-#undef SERIAL_7E2
-#undef SERIAL_8E2
-#undef SERIAL_5O1
-#undef SERIAL_6O1
-#undef SERIAL_7O1
-#undef SERIAL_8O1
-#undef SERIAL_5O2
-#undef SERIAL_6O2
-#undef SERIAL_7O2
-#undef SERIAL_8O2
-
-// Define config for Serial.begin(baud, config);
-#define SERIAL_5N1 0x00
-#define SERIAL_6N1 0x02
-#define SERIAL_7N1 0x04
-#define SERIAL_8N1 0x06
-#define SERIAL_5N2 0x08
-#define SERIAL_6N2 0x0A
-#define SERIAL_7N2 0x0C
-#define SERIAL_8N2 0x0E
-#define SERIAL_5E1 0x20
-#define SERIAL_6E1 0x22
-#define SERIAL_7E1 0x24
-#define SERIAL_8E1 0x26
-#define SERIAL_5E2 0x28
-#define SERIAL_6E2 0x2A
-#define SERIAL_7E2 0x2C
-#define SERIAL_8E2 0x2E
-#define SERIAL_5O1 0x30
-#define SERIAL_6O1 0x32
-#define SERIAL_7O1 0x34
-#define SERIAL_8O1 0x36
-#define SERIAL_5O2 0x38
-#define SERIAL_6O2 0x3A
-#define SERIAL_7O2 0x3C
-#define SERIAL_8O2 0x3E
-
 typedef enum {
   TX_STARTED,
   TX_STOPPED
