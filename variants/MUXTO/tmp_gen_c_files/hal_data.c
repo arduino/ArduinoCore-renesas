b084ac23470e7bfd8f2d5b78be78bc71823f9fbb
Martino Facchin <m.facchin@arduino.cc>
2022-09-30 12:29:52 +0200

    muxto: add generated files
    Former-commit-id: e17a1310de9671f67843de325ef2062dae118624

---
+++ variants/MUXTO/tmp_gen_c_files/hal_data.c
@@ -0,0 +1,411 @@
+/* generated HAL source file - do not edit */
+#include "hal_data.h"
+/* Macros to tie dynamic ELC links to ADC_TRIGGER_SYNC_ELC option in adc_trigger_t. */
+#define ADC_TRIGGER_ADC0        ADC_TRIGGER_SYNC_ELC
+#define ADC_TRIGGER_ADC0_B      ADC_TRIGGER_SYNC_ELC
+#define ADC_TRIGGER_ADC1        ADC_TRIGGER_SYNC_ELC
+#define ADC_TRIGGER_ADC1_B      ADC_TRIGGER_SYNC_ELC
+
+adc_instance_ctrl_t g_adc0_ctrl;
+const adc_extended_cfg_t g_adc0_cfg_extend =
+{ .add_average_count = ADC_ADD_OFF,
+  .clearing = ADC_CLEAR_AFTER_READ_ON,
+  .trigger_group_b = ADC_TRIGGER_SYNC_ELC,
+  .double_trigger_mode = ADC_DOUBLE_TRIGGER_DISABLED,
+  .adc_vref_control = ADC_VREF_CONTROL_VREFH,
+  .enable_adbuf = 0,
+#if defined(VECTOR_NUMBER_ADC0_WINDOW_A)
+    .window_a_irq        = VECTOR_NUMBER_ADC0_WINDOW_A,
+#else
+  .window_a_irq = FSP_INVALID_VECTOR,
+#endif
+  .window_a_ipl = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_ADC0_WINDOW_B)
+    .window_b_irq      = VECTOR_NUMBER_ADC0_WINDOW_B,
+#else
+  .window_b_irq = FSP_INVALID_VECTOR,
+#endif
+  .window_b_ipl = (BSP_IRQ_DISABLED), };
+const adc_cfg_t g_adc0_cfg =
+{ .unit = 0,
+  .mode = ADC_MODE_SINGLE_SCAN,
+  .resolution = ADC_RESOLUTION_16_BIT,
+  .alignment = (adc_alignment_t) (0),
+  .trigger = ADC_TRIGGER_SOFTWARE,
+  .p_callback = NULL,
+  /** If NULL then do not add & */
+#if defined(NULL)
+    .p_context           = NULL,
+#else
+  .p_context = &NULL,
+#endif
+  .p_extend = &g_adc0_cfg_extend,
+#if defined(VECTOR_NUMBER_ADC0_SCAN_END)
+    .scan_end_irq        = VECTOR_NUMBER_ADC0_SCAN_END,
+#else
+  .scan_end_irq = FSP_INVALID_VECTOR,
+#endif
+  .scan_end_ipl = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_ADC0_SCAN_END_B)
+    .scan_end_b_irq      = VECTOR_NUMBER_ADC0_SCAN_END_B,
+#else
+  .scan_end_b_irq = FSP_INVALID_VECTOR,
+#endif
+  .scan_end_b_ipl = (BSP_IRQ_DISABLED), };
+#if ((0) | (0))
+const adc_window_cfg_t g_adc0_window_cfg =
+{
+    .compare_mask        =  0,
+    .compare_mode_mask   =  0,
+    .compare_cfg         = (0) | (0) | (0) | (ADC_COMPARE_CFG_EVENT_OUTPUT_OR),
+    .compare_ref_low     = 0,
+    .compare_ref_high    = 0,
+    .compare_b_channel   = (ADC_WINDOW_B_CHANNEL_0),
+    .compare_b_mode      = (ADC_WINDOW_B_MODE_LESS_THAN_OR_OUTSIDE),
+    .compare_b_ref_low   = 0,
+    .compare_b_ref_high  = 0,
+};
+#endif
+const adc_channel_cfg_t g_adc0_channel_cfg =
+{ .scan_mask = 0,
+  .scan_mask_group_b = 0,
+  .priority_group_a = ADC_GROUP_A_PRIORITY_OFF,
+  .add_mask = 0,
+  .sample_hold_mask = 0,
+  .sample_hold_states = 24,
+#if ((0) | (0))
+    .p_window_cfg        = (adc_window_cfg_t *) &g_adc0_window_cfg,
+#else
+  .p_window_cfg = NULL,
+#endif
+        };
+/* Instance structure to use this module. */
+const adc_instance_t g_adc0 =
+{ .p_ctrl = &g_adc0_ctrl, .p_cfg = &g_adc0_cfg, .p_channel_cfg = &g_adc0_channel_cfg, .p_api = &g_adc_on_adc };
+gpt_instance_ctrl_t g_timer1_ctrl;
+#if 0
+const gpt_extended_pwm_cfg_t g_timer1_pwm_extend =
+{
+    .trough_ipl          = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_GPT0_COUNTER_UNDERFLOW)
+    .trough_irq          = VECTOR_NUMBER_GPT0_COUNTER_UNDERFLOW,
+#else
+    .trough_irq          = FSP_INVALID_VECTOR,
+#endif
+    .poeg_link           = GPT_POEG_LINK_POEG0,
+    .output_disable      =  GPT_OUTPUT_DISABLE_NONE,
+    .adc_trigger         =  GPT_ADC_TRIGGER_NONE,
+    .dead_time_count_up  = 0,
+    .dead_time_count_down = 0,
+    .adc_a_compare_match = 0,
+    .adc_b_compare_match = 0,
+    .interrupt_skip_source = GPT_INTERRUPT_SKIP_SOURCE_NONE,
+    .interrupt_skip_count  = GPT_INTERRUPT_SKIP_COUNT_0,
+    .interrupt_skip_adc    = GPT_INTERRUPT_SKIP_ADC_NONE,
+    .gtioca_disable_setting = GPT_GTIOC_DISABLE_PROHIBITED,
+    .gtiocb_disable_setting = GPT_GTIOC_DISABLE_PROHIBITED,
+};
+#endif
+const gpt_extended_cfg_t g_timer1_extend =
+        { .gtioca =
+        { .output_enabled = false, .stop_level = GPT_PIN_LEVEL_LOW },
+          .gtiocb =
+          { .output_enabled = false, .stop_level = GPT_PIN_LEVEL_LOW },
+          .start_source = (gpt_source_t) (GPT_SOURCE_NONE), .stop_source = (gpt_source_t) (GPT_SOURCE_NONE), .clear_source =
+                  (gpt_source_t) (GPT_SOURCE_NONE),
+          .count_up_source = (gpt_source_t) (GPT_SOURCE_NONE), .count_down_source = (gpt_source_t) (GPT_SOURCE_NONE), .capture_a_source =
+                  (gpt_source_t) (GPT_SOURCE_NONE),
+          .capture_b_source = (gpt_source_t) (GPT_SOURCE_NONE), .capture_a_ipl = (BSP_IRQ_DISABLED), .capture_b_ipl =
+                  (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_GPT0_CAPTURE_COMPARE_A)
+    .capture_a_irq       = VECTOR_NUMBER_GPT0_CAPTURE_COMPARE_A,
+#else
+          .capture_a_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_GPT0_CAPTURE_COMPARE_B)
+    .capture_b_irq       = VECTOR_NUMBER_GPT0_CAPTURE_COMPARE_B,
+#else
+          .capture_b_irq = FSP_INVALID_VECTOR,
+#endif
+          .capture_filter_gtioca = GPT_CAPTURE_FILTER_NONE,
+          .capture_filter_gtiocb = GPT_CAPTURE_FILTER_NONE,
+#if 0
+    .p_pwm_cfg                   = &g_timer1_pwm_extend,
+#else
+          .p_pwm_cfg = NULL,
+#endif
+#if 0
+    .gtior_setting.gtior_b.gtioa  = (0U << 4U) | (0U << 2U) | (0U << 0U),
+    .gtior_setting.gtior_b.oadflt = (uint32_t) GPT_PIN_LEVEL_LOW,
+    .gtior_setting.gtior_b.oahld  = 0U,
+    .gtior_setting.gtior_b.oae    = (uint32_t) false,
+    .gtior_setting.gtior_b.oadf   = (uint32_t) GPT_GTIOC_DISABLE_PROHIBITED,
+    .gtior_setting.gtior_b.nfaen  = ((uint32_t) GPT_CAPTURE_FILTER_NONE & 1U),
+    .gtior_setting.gtior_b.nfcsa  = ((uint32_t) GPT_CAPTURE_FILTER_NONE >> 1U),
+    .gtior_setting.gtior_b.gtiob  = (0U << 4U) | (0U << 2U) | (0U << 0U),
+    .gtior_setting.gtior_b.obdflt = (uint32_t) GPT_PIN_LEVEL_LOW,
+    .gtior_setting.gtior_b.obhld  = 0U,
+    .gtior_setting.gtior_b.obe    = (uint32_t) false,
+    .gtior_setting.gtior_b.obdf   = (uint32_t) GPT_GTIOC_DISABLE_PROHIBITED,
+    .gtior_setting.gtior_b.nfben  = ((uint32_t) GPT_CAPTURE_FILTER_NONE & 1U),
+    .gtior_setting.gtior_b.nfcsb  = ((uint32_t) GPT_CAPTURE_FILTER_NONE >> 1U),
+#else
+          .gtior_setting.gtior = 0U,
+#endif
+        };
+const timer_cfg_t g_timer1_cfg =
+{ .mode = TIMER_MODE_PERIODIC,
+/* Actual period: 89.47848533333334 seconds. Actual duty: 50%. */.period_counts = (uint32_t) 0x100000000,
+  .duty_cycle_counts = 0x80000000, .source_div = (timer_source_div_t) 0, .channel = 0, .p_callback = NULL,
+  /** If NULL then do not add & */
+#if defined(NULL)
+    .p_context           = NULL,
+#else
+  .p_context = &NULL,
+#endif
+  .p_extend = &g_timer1_extend,
+  .cycle_end_ipl = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_GPT0_COUNTER_OVERFLOW)
+    .cycle_end_irq       = VECTOR_NUMBER_GPT0_COUNTER_OVERFLOW,
+#else
+  .cycle_end_irq = FSP_INVALID_VECTOR,
+#endif
+        };
+/* Instance structure to use this module. */
+const timer_instance_t g_timer1 =
+{ .p_ctrl = &g_timer1_ctrl, .p_cfg = &g_timer1_cfg, .p_api = &g_timer_on_gpt };
+agt_instance_ctrl_t g_timer0_ctrl;
+const agt_extended_cfg_t g_timer0_extend =
+{ .count_source = AGT_CLOCK_PCLKB,
+  .agto = AGT_PIN_CFG_DISABLED,
+  .agtoab_settings_b.agtoa = AGT_PIN_CFG_DISABLED,
+  .agtoab_settings_b.agtob = AGT_PIN_CFG_DISABLED,
+  .measurement_mode = AGT_MEASURE_DISABLED,
+  .agtio_filter = AGT_AGTIO_FILTER_NONE,
+  .enable_pin = AGT_ENABLE_PIN_NOT_USED,
+  .trigger_edge = AGT_TRIGGER_EDGE_RISING, };
+const timer_cfg_t g_timer0_cfg =
+{ .mode = TIMER_MODE_PERIODIC,
+/* Actual period: 0.002730666666666667 seconds. Actual duty: 50%. */.period_counts = (uint32_t) 0x10000,
+  .duty_cycle_counts = 0x8000, .source_div = (timer_source_div_t) 0, .channel = 0, .p_callback = NULL,
+  /** If NULL then do not add & */
+#if defined(NULL)
+    .p_context           = NULL,
+#else
+  .p_context = &NULL,
+#endif
+  .p_extend = &g_timer0_extend,
+  .cycle_end_ipl = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_AGT0_INT)
+    .cycle_end_irq       = VECTOR_NUMBER_AGT0_INT,
+#else
+  .cycle_end_irq = FSP_INVALID_VECTOR,
+#endif
+        };
+/* Instance structure to use this module. */
+const timer_instance_t g_timer0 =
+{ .p_ctrl = &g_timer0_ctrl, .p_cfg = &g_timer0_cfg, .p_api = &g_timer_on_agt };
+usb_instance_ctrl_t g_basic0_ctrl;
+
+#if !defined(g_usb_descriptor)
+extern usb_descriptor_t g_usb_descriptor;
+#endif
+#define RA_NOT_DEFINED (1)
+const usb_cfg_t g_basic0_cfg =
+{ .usb_mode = USB_MODE_PERI,
+  .usb_speed = USB_SPEED_FS,
+  .module_number = 0,
+  .type = USB_CLASS_PCDC,
+#if defined(g_usb_descriptor)
+                .p_usb_reg = g_usb_descriptor,
+#else
+  .p_usb_reg = &g_usb_descriptor,
+#endif
+  .usb_complience_cb = NULL,
+#if defined(VECTOR_NUMBER_USBFS_INT)
+                .irq       = VECTOR_NUMBER_USBFS_INT,
+#else
+  .irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_USBFS_RESUME)
+                .irq_r     = VECTOR_NUMBER_USBFS_RESUME,
+#else
+  .irq_r = FSP_INVALID_VECTOR,
+#endif
+  .irq_d0 = FSP_INVALID_VECTOR,
+  .irq_d1 = FSP_INVALID_VECTOR,
+  .hsirq = FSP_INVALID_VECTOR,
+  .hsirq_d0 = FSP_INVALID_VECTOR,
+  .hsirq_d1 = FSP_INVALID_VECTOR,
+  .ipl = (2),
+  .ipl_r = (2),
+  .ipl_d0 = FSP_INVALID_VECTOR,
+  .ipl_d1 = FSP_INVALID_VECTOR,
+  .hsipl = FSP_INVALID_VECTOR,
+  .hsipl_d0 = FSP_INVALID_VECTOR,
+  .hsipl_d1 = FSP_INVALID_VECTOR,
+#if (BSP_CFG_RTOS != 0)
+                .p_usb_apl_callback = NULL,
+#else
+  .p_usb_apl_callback = NULL,
+#endif
+#if defined(NULL)
+                .p_context = NULL,
+#else
+  .p_context = &NULL,
+#endif
+        };
+#undef RA_NOT_DEFINED
+
+/* Instance structure to use this module. */
+const usb_instance_t g_basic0 =
+{ .p_ctrl = &g_basic0_ctrl, .p_cfg = &g_basic0_cfg, .p_api = &g_usb_on_usb, };
+
+sci_uart_instance_ctrl_t g_uart1_ctrl;
+
+baud_setting_t g_uart1_baud_setting =
+        {
+        /* Baud rate calculated with 0.160% error. */.semr_baudrate_bits_b.abcse = 0,
+          .semr_baudrate_bits_b.abcs = 0, .semr_baudrate_bits_b.bgdm = 1, .cks = 0, .brr = 12, .mddr = (uint8_t) 256, .semr_baudrate_bits_b.brme =
+                  false };
+
+/** UART extended configuration for UARTonSCI HAL driver */
+const sci_uart_extended_cfg_t g_uart1_cfg_extend =
+{ .clock = SCI_UART_CLOCK_INT, .rx_edge_start = SCI_UART_START_BIT_FALLING_EDGE, .noise_cancel =
+          SCI_UART_NOISE_CANCELLATION_DISABLE,
+  .rx_fifo_trigger = SCI_UART_RX_FIFO_TRIGGER_MAX, .p_baud_setting = &g_uart1_baud_setting, .flow_control =
+          SCI_UART_FLOW_CONTROL_RTS,
+#if 0xFF != 0xFF
+                .flow_control_pin       = BSP_IO_PORT_FF_PIN_0xFF,
+                #else
+  .flow_control_pin = (bsp_io_port_pin_t) UINT16_MAX,
+#endif
+  .rs485_setting =
+  { .enable = SCI_UART_RS485_DISABLE, .polarity = SCI_UART_RS485_DE_POLARITY_HIGH,
+#if 0xFF != 0xFF
+                    .de_control_pin = BSP_IO_PORT_FF_PIN_0xFF,
+                #else
+    .de_control_pin = (bsp_io_port_pin_t) UINT16_MAX,
+#endif
+          }, };
+
+/** UART interface configuration */
+const uart_cfg_t g_uart1_cfg =
+{ .channel = 0, .data_bits = UART_DATA_BITS_8, .parity = UART_PARITY_OFF, .stop_bits = UART_STOP_BITS_1, .p_callback =
+          NULL,
+  .p_context = NULL, .p_extend = &g_uart1_cfg_extend,
+#define RA_NOT_DEFINED (1)
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_tx = NULL,
+#else
+                .p_transfer_tx       = &RA_NOT_DEFINED,
+#endif
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_rx = NULL,
+#else
+                .p_transfer_rx       = &RA_NOT_DEFINED,
+#endif
+#undef RA_NOT_DEFINED
+  .rxi_ipl = (2),
+  .txi_ipl = (2), .tei_ipl = (2), .eri_ipl = (2),
+#if defined(VECTOR_NUMBER_SCI0_RXI)
+                .rxi_irq             = VECTOR_NUMBER_SCI0_RXI,
+#else
+  .rxi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI0_TXI)
+                .txi_irq             = VECTOR_NUMBER_SCI0_TXI,
+#else
+  .txi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI0_TEI)
+                .tei_irq             = VECTOR_NUMBER_SCI0_TEI,
+#else
+  .tei_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI0_ERI)
+                .eri_irq             = VECTOR_NUMBER_SCI0_ERI,
+#else
+  .eri_irq = FSP_INVALID_VECTOR,
+#endif
+        };
+
+/* Instance structure to use this module. */
+const uart_instance_t g_uart1 =
+{ .p_ctrl = &g_uart1_ctrl, .p_cfg = &g_uart1_cfg, .p_api = &g_uart_on_sci };
+sci_uart_instance_ctrl_t g_uart0_ctrl;
+
+baud_setting_t g_uart0_baud_setting =
+        {
+        /* Baud rate calculated with 0.160% error. */.semr_baudrate_bits_b.abcse = 0,
+          .semr_baudrate_bits_b.abcs = 0, .semr_baudrate_bits_b.bgdm = 1, .cks = 0, .brr = 12, .mddr = (uint8_t) 256, .semr_baudrate_bits_b.brme =
+                  false };
+
+/** UART extended configuration for UARTonSCI HAL driver */
+const sci_uart_extended_cfg_t g_uart0_cfg_extend =
+{ .clock = SCI_UART_CLOCK_INT, .rx_edge_start = SCI_UART_START_BIT_FALLING_EDGE, .noise_cancel =
+          SCI_UART_NOISE_CANCELLATION_DISABLE,
+  .rx_fifo_trigger = SCI_UART_RX_FIFO_TRIGGER_MAX, .p_baud_setting = &g_uart0_baud_setting, .flow_control =
+          SCI_UART_FLOW_CONTROL_RTS,
+#if 0xFF != 0xFF
+                .flow_control_pin       = BSP_IO_PORT_FF_PIN_0xFF,
+                #else
+  .flow_control_pin = (bsp_io_port_pin_t) UINT16_MAX,
+#endif
+  .rs485_setting =
+  { .enable = SCI_UART_RS485_DISABLE, .polarity = SCI_UART_RS485_DE_POLARITY_HIGH,
+#if 0xFF != 0xFF
+                    .de_control_pin = BSP_IO_PORT_FF_PIN_0xFF,
+                #else
+    .de_control_pin = (bsp_io_port_pin_t) UINT16_MAX,
+#endif
+          }, };
+
+/** UART interface configuration */
+const uart_cfg_t g_uart0_cfg =
+{ .channel = 0, .data_bits = UART_DATA_BITS_8, .parity = UART_PARITY_OFF, .stop_bits = UART_STOP_BITS_1, .p_callback =
+          NULL,
+  .p_context = NULL, .p_extend = &g_uart0_cfg_extend,
+#define RA_NOT_DEFINED (1)
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_tx = NULL,
+#else
+                .p_transfer_tx       = &RA_NOT_DEFINED,
+#endif
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_rx = NULL,
+#else
+                .p_transfer_rx       = &RA_NOT_DEFINED,
+#endif
+#undef RA_NOT_DEFINED
+  .rxi_ipl = (2),
+  .txi_ipl = (2), .tei_ipl = (2), .eri_ipl = (2),
+#if defined(VECTOR_NUMBER_SCI0_RXI)
+                .rxi_irq             = VECTOR_NUMBER_SCI0_RXI,
+#else
+  .rxi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI0_TXI)
+                .txi_irq             = VECTOR_NUMBER_SCI0_TXI,
+#else
+  .txi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI0_TEI)
+                .tei_irq             = VECTOR_NUMBER_SCI0_TEI,
+#else
+  .tei_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI0_ERI)
+                .eri_irq             = VECTOR_NUMBER_SCI0_ERI,
+#else
+  .eri_irq = FSP_INVALID_VECTOR,
+#endif
+        };
+
+/* Instance structure to use this module. */
+const uart_instance_t g_uart0 =
+{ .p_ctrl = &g_uart0_ctrl, .p_cfg = &g_uart0_cfg, .p_api = &g_uart_on_sci };
+void g_hal_init(void)
+{
+    g_common_init ();
+}
