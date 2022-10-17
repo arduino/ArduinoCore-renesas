a3b165c64dd22012537bff81bde77967cbcd9ef4
Martino Facchin <m.facchin@arduino.cc>
2022-10-17 20:42:30 +0200

    h33: fix interrupt security
    Former-commit-id: 04c2cd942886e3d398d7a5b995d11bf9368f03a6

--- cores/arduino/main.cpp
+++ cores/arduino/main.cpp
@@ -19,8 +19,47 @@ extern uint32_t __ROM_Start;
 extern const fsp_vector_t __VECTOR_TABLE[];
 extern const fsp_vector_t g_vector_table[];
 
-extern "C" void SysTick_Handler(void) {}
-extern "C" void HardFault_Handler(void) {}
+
+#if FSP_PRIV_TZ_USE_SECURE_REGS
+
+#define BSP_PRV_BITS_PER_WORD    (32)
+
+void unsecure_registers() {
+
+    /* Unprotect security registers. */
+    R_BSP_RegisterProtectDisable(BSP_REG_PROTECT_SAR);
+
+ #if !BSP_TZ_SECURE_BUILD
+    /* Set the DMAC channels to secure access. */
+    R_CPSCU->ICUSARC = ~R_CPSCU_ICUSARC_SADMACn_Msk;
+ #endif
+
+    /* Place all vectors in non-secure state unless they are used in the secure project. */
+    uint32_t interrupt_security_state[BSP_ICU_VECTOR_MAX_ENTRIES / BSP_PRV_BITS_PER_WORD];
+    memset(&interrupt_security_state, UINT8_MAX, sizeof(interrupt_security_state));
+
+    for (uint32_t i = 0U; i < BSP_ICU_VECTOR_MAX_ENTRIES; i++)
+    {
+         /* This is a secure vector. Clear the associated bit. */
+         uint32_t index = i / BSP_PRV_BITS_PER_WORD;
+         uint32_t bit   = i % BSP_PRV_BITS_PER_WORD;
+         interrupt_security_state[index] &= ~(1U << bit);
+    }
+
+    /* The Secure Attribute managed within the ARM CPU NVIC must match the security attribution of IELSEn
+     * (Reference section 13.2.9 in the RA6M4 manual R01UH0890EJ0050). */
+    uint32_t volatile * p_icusarg = &R_CPSCU->ICUSARG;
+    for (uint32_t i = 0U; i < BSP_ICU_VECTOR_MAX_ENTRIES / BSP_PRV_BITS_PER_WORD; i++)
+    {
+        p_icusarg[i]  = interrupt_security_state[i];
+        NVIC->ITNS[i] = interrupt_security_state[i];
+    }
+
+    /* Protect security registers. */
+    R_BSP_RegisterProtectEnable(BSP_REG_PROTECT_SAR);
+}
+#endif
+
 
 void arduino_main(void)
 {
@@ -30,6 +69,7 @@ void arduino_main(void)
    // When stack and heap would collide, we could signal the NMI with mbed style leds patterns
 #if defined(__ARM_ARCH_8M_MAIN__) || defined(__ARM_ARCH_8M_BASE__)
    __set_MSPLIM(0);
+   unsecure_registers();
 #else
    R_MPU_SPMON->SP[0].CTL = 0;
 #endif
