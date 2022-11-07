2c8f62d26053cacfe48e1ba84314f7ba70cf1ea7
Alexander Entinger <cto@lxrobotics.com>
2022-11-07 10:26:00 +0100

    Temp storage.
    Former-commit-id: ecdd18d89fb48f8c2c3afe97210e816e48064ddb

--- libraries/CAN/examples/CANWrite/CANWrite.ino
+++ libraries/CAN/examples/CANWrite/CANWrite.ino
@@ -27,26 +27,30 @@ void setup()
     //Serial.println(" failed :(");
     for (;;) {}
   }
-  /*
+/*
   if (CAN.enableInternalLoopback() < 0)
   {
     for(;;) { }
   }
-  */
+*/
 
   //Serial.println(" OK :)");
 }
 
+static uint8_t msg_cnt = 0;
+
 void loop()
 {
-  uint8_t const msg_data[] = {1,2,3,4,5,6,7,8};
-  CanMsg msg = {CAN_ID, sizeof(msg_data), msg_data};
+  uint8_t const msg_data[] = {1,2,3,4,5,6,7,msg_cnt};
+  CanMsg msg(CAN_ID, sizeof(msg_data), msg_data);
 
   if (CAN.write(msg) < 0)
   {
     //Serial.println("Message write failed :(");
     for (;;) {}
   }
+
+  msg_cnt++;
 
   delay(1000);
 }
