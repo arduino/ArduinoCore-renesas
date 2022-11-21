b44ce0c2ab538d6eab2cc5711a794748345a9391
Martino Facchin <m.facchin@arduino.cc>
2022-11-21 19:52:42 +0100

    USB: implement PluggableHID
    Should support old Keyboard and Mouse libraries
    
    
    Former-commit-id: e2a637dd231620852304d691f6778ac413bfe8da

--- cores/arduino/usb/USB.cpp
+++ cores/arduino/usb/USB.cpp
@@ -21,10 +21,12 @@
 #include "IRQManager.h"
 #include "USB.h"
 
+extern "C" {
 #include "tusb.h"
 #include "class/hid/hid_device.h"
 #include "class/audio/audio.h"
 #include "class/midi/midi.h"
+}
 
 #include "r_usb_basic.h"
 #include "r_usb_basic_api.h"
@@ -33,11 +35,12 @@
 #define USBD_DESC_LEN (TUD_CONFIG_DESC_LEN + TUD_CDC_DESC_LEN)
 
 #define USBD_ITF_CDC (0) // needs 2 interfaces
-#define USBD_ITF_MAX (2)
+#define USBD_ITF_MAX (3)
 
 #define USBD_CDC_EP_CMD (0x81)
 #define USBD_CDC_EP_OUT (0x02)
 #define USBD_CDC_EP_IN (0x82)
+#define USBD_HID_EP    (0x83)
 #define USBD_CDC_CMD_MAX_SIZE (8)
 #if (CFG_TUSB_RHPORT1_MODE & OPT_MODE_DEVICE)
 #define USBD_CDC_IN_OUT_MAX_SIZE (512)
@@ -50,12 +53,6 @@
 #define USBD_STR_PRODUCT (0x02)
 #define USBD_STR_SERIAL (0x03)
 #define USBD_STR_CDC (0x04)
-
-
-#define EPNUM_HID   0x83
-
-#define EPNUM_MIDI   0x01
-
 
 const uint8_t *tud_descriptor_device_cb(void) {
     static tusb_desc_device_t usbd_desc_device = {
@@ -74,78 +71,12 @@ const uint8_t *tud_descriptor_device_cb(void) {
         .iSerialNumber = USBD_STR_SERIAL,
         .bNumConfigurations = 1
     };
-    if (__USBInstallSerial && !__USBInstallKeyboard && !__USBInstallMouse && !__USBInstallMIDI) {
-        // Can use as-is, this is the default USB case
-        return (const uint8_t *)&usbd_desc_device;
-    }
-    // Need a multi-endpoint config which will require changing the PID to help Windows not barf
-    if (__USBInstallKeyboard) {
-        usbd_desc_device.idProduct |= 0x8000;
-    }
-    if (__USBInstallMouse) {
-        usbd_desc_device.idProduct |= 0x4000;
-    }
-    if (__USBInstallMIDI) {
-        usbd_desc_device.idProduct |= 0x2000;
-    }
-    // Set the device class to 0 to indicate multiple device classes
+
+    // Descriptors are always composite
     usbd_desc_device.bDeviceClass = 0;
     usbd_desc_device.bDeviceSubClass = 0;
     usbd_desc_device.bDeviceProtocol = 0;
     return (const uint8_t *)&usbd_desc_device;
-}
-
-int __USBGetKeyboardReportID() {
-    return 1;
-}
-
-int __USBGetMouseReportID() {
-    return __USBInstallKeyboard ? 2 : 1;
-}
-
-static int      __hid_report_len = 0;
-static uint8_t *__hid_report     = nullptr;
-
-static uint8_t *GetDescHIDReport(int *len) {
-    if (len) {
-        *len = __hid_report_len;
-    }
-    return __hid_report;
-}
-
-void __SetupDescHIDReport() {
-    if (__USBInstallKeyboard && __USBInstallMouse) {
-        uint8_t desc_hid_report[] = {
-            TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(1)),
-            TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(2))
-        };
-        __hid_report = (uint8_t *)malloc(sizeof(desc_hid_report));
-        if (__hid_report) {
-            __hid_report_len = sizeof(desc_hid_report);
-            memcpy(__hid_report, desc_hid_report, __hid_report_len);
-        }
-    } else if (__USBInstallKeyboard && ! __USBInstallMouse) {
-        uint8_t desc_hid_report[] = {
-            TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(1))
-        };
-        __hid_report = (uint8_t *)malloc(sizeof(desc_hid_report));
-        if (__hid_report) {
-            __hid_report_len = sizeof(desc_hid_report);
-            memcpy(__hid_report, desc_hid_report, __hid_report_len);
-        }
-    } else if (! __USBInstallKeyboard &&  __USBInstallMouse) {
-        uint8_t desc_hid_report[] = {
-            TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(1))
-        };
-        __hid_report = (uint8_t *)malloc(sizeof(desc_hid_report));
-        if (__hid_report) {
-            __hid_report_len = sizeof(desc_hid_report);
-            memcpy(__hid_report, desc_hid_report, __hid_report_len);
-        }
-    } else {
-        __hid_report = nullptr;
-        __hid_report_len = 0;
-    }
 }
 
 // Invoked when received GET HID REPORT DESCRIPTOR
@@ -153,7 +84,12 @@ void __SetupDescHIDReport() {
 // Descriptor contents must exist long enough for transfer to complete
 uint8_t const * tud_hid_descriptor_report_cb(uint8_t instance) {
     (void) instance;
-    return GetDescHIDReport(nullptr);
+    size_t len = 0;
+    if (__USBGetHIDReport) {
+        return __USBGetHIDReport(&len);
+    } else {
+        return NULL;
+    }
 }
 
 static uint8_t *usbd_desc_cfg = nullptr;
@@ -164,30 +100,25 @@ const uint8_t *tud_descriptor_configuration_cb(uint8_t index) {
 
 void __SetupUSBDescriptor() {
     if (!usbd_desc_cfg) {
-        bool hasHID = __USBInstallKeyboard || __USBInstallMouse;
 
-        uint8_t interface_count = (__USBInstallSerial ? 2 : 0) + (hasHID ? 1 : 0) + (__USBInstallMIDI ? 2 : 0);
+        uint8_t interface_count = (__USBInstallSerial ? 2 : 0) + (__USBGetHIDReport ? 1 : 0);
 
         uint8_t cdc_desc[TUD_CDC_DESC_LEN] = {
             // Interface number, string index, protocol, report descriptor len, EP In & Out address, size & polling interval
             TUD_CDC_DESCRIPTOR(USBD_ITF_CDC, USBD_STR_CDC, USBD_CDC_EP_CMD, USBD_CDC_CMD_MAX_SIZE, USBD_CDC_EP_OUT, USBD_CDC_EP_IN, USBD_CDC_IN_OUT_MAX_SIZE)
         };
 
-        int hid_report_len;
-        GetDescHIDReport(&hid_report_len);
+        size_t hid_report_len = 0;
+        if (__USBGetHIDReport) {
+            __USBGetHIDReport(&hid_report_len);
+        }
         uint8_t hid_itf = __USBInstallSerial ? 2 : 0;
         uint8_t hid_desc[TUD_HID_DESC_LEN] = {
             // Interface number, string index, protocol, report descriptor len, EP In & Out address, size & polling interval
-            TUD_HID_DESCRIPTOR(hid_itf, 0, HID_ITF_PROTOCOL_NONE, hid_report_len, EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 10)
+            TUD_HID_DESCRIPTOR(hid_itf, 0, HID_ITF_PROTOCOL_NONE, hid_report_len, USBD_HID_EP, CFG_TUD_HID_EP_BUFSIZE, 10)
         };
 
-        uint8_t midi_itf = hid_itf + (hasHID ? 1 : 0);
-        uint8_t midi_desc[TUD_MIDI_DESC_LEN] = {
-            // Interface number, string index, EP Out & EP In address, EP size
-            TUD_MIDI_DESCRIPTOR(midi_itf, 0, EPNUM_MIDI, 0x80 | EPNUM_MIDI, 64)
-        };
-
-        int usbd_desc_len = TUD_CONFIG_DESC_LEN + (__USBInstallSerial ? sizeof(cdc_desc) : 0) + (hasHID ? sizeof(hid_desc) : 0) + (__USBInstallMIDI ? sizeof(midi_desc) : 0);
+        int usbd_desc_len = TUD_CONFIG_DESC_LEN + (__USBInstallSerial ? sizeof(cdc_desc) : 0) + (__USBGetHIDReport ? sizeof(hid_desc) : 0);
 
         uint8_t tud_cfg_desc[TUD_CONFIG_DESC_LEN] = {
             // Config number, interface count, string index, total length, attribute, power in mA
@@ -205,12 +136,9 @@ void __SetupUSBDescriptor() {
                 memcpy(ptr, cdc_desc, sizeof(cdc_desc));
                 ptr += sizeof(cdc_desc);
             }
-            if (hasHID) {
+            if (__USBGetHIDReport) {
                 memcpy(ptr, hid_desc, sizeof(hid_desc));
                 ptr += sizeof(hid_desc);
-            }
-            if (__USBInstallMIDI) {
-                memcpy(ptr, midi_desc, sizeof(midi_desc));
             }
         }
     }
@@ -351,7 +279,6 @@ void __USBStart() {
 #endif
 #endif
 
-    __SetupDescHIDReport();
     __SetupUSBDescriptor();
 
     tud_init(BOARD_TUD_RHPORT);
