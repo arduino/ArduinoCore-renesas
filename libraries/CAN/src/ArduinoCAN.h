e88f497d1e760465b0cd6699a2cf6d291bcdaf71
Alexander Entinger <cto@lxrobotics.com>
2022-11-23 07:06:00 +0100

    Use top-level-header ArduinoCAN to determine whether to include CAN.h or CANFD.h
    Former-commit-id: 2f8227be3cead1e3cf2ed2d62148e7f87852456c

---
+++ libraries/CAN/src/ArduinoCAN.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022 by Alexander Entinger <a.entinger@arduino.cc>
+ * CAN library for Arduino.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef ARDUINO_CORE_RENESAS_ARDUINO_CAN_LIBRARY_H_
+#define ARDUINO_CORE_RENESAS_ARDUINO_CAN_LIBRARY_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <Arduino.h>
+
+#if CAN_HOWMANY > 0
+# include "CAN.h"
+#endif
+
+#if CANFD_HOWMANY > 0
+# include "CANFD.h"
+#endif
+
+#endif /* ARDUINO_CORE_RENESAS_ARDUINO_CAN_LIBRARY_H_ */
