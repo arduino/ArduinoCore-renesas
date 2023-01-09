0922850b852a628c4b1d9fc64f6af2fa827d2e98
Martino Facchin <m.facchin@arduino.cc>
2023-01-09 10:03:32 +0100

    TEMP: Santiago: set leds as OUTPUT, LOW at startup
    It would be better to solve this in hardware
    
    
    Former-commit-id: 94c75dc55139010977e264b5639228d65f8bd430

--- variants/SANTIAGO/variant.cpp
+++ variants/SANTIAGO/variant.cpp
@@ -138,7 +138,12 @@ int32_t getPinIndex(bsp_io_port_pin_t p) {
 
 void initVariant() {
   // bootloader configures LED_BUILTIN as PWM output, deconfigure it to avoid spurious signals
-  pinMode(LED_BUILTIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  pinMode(LEDR, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LEDB, LOW);
+  digitalWrite(LEDR, LOW);
   FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfg(g_pin_cfg[3].list, PIN_CFG_REQ_PWM)));
   FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfg(g_pin_cfg[5].list, PIN_CFG_REQ_PWM)));
   FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfg(g_pin_cfg[6].list, PIN_CFG_REQ_PWM)));
