a44c9b6c04652c96ffe06130660848f179692590
Martino Facchin <m.facchin@arduino.cc>
2023-02-14 19:16:41 +0100

    Add Composta
    Former-commit-id: 125504ed0f8649f6819b4a3206e1d160d195d17f

---
+++ variants/SANTIAGO_COMPOSTA/variant.cpp
@@ -0,0 +1,154 @@
+#include "Arduino.h"
+#include "pinmux.inc"
+
+const uint16_t P400_b[] = {
+PIN_PWM|CHANNEL_6|PWM_CHANNEL_A|GPT_ODD_CFG,
+PIN_SCL|CHANNEL_0,
+PIN_INTERRUPT|CHANNEL_0,
+SCI_CHANNEL|PIN_SCK|CHANNEL_0|SCI_EVEN_CFG|LAST_ITEM_GUARD
+};
+#define P400 P400_b
+
+const uint16_t P408_b[] = {
+PIN_PWM|CHANNEL_5|PWM_CHANNEL_B|GPT_ODD_CFG,
+PIN_INTERRUPT|CHANNEL_7,
+SCI_CHANNEL|PIN_CTS_RTS_SS|CHANNEL_1|SCI_EVEN_CFG,
+SCI_CHANNEL|PIN_RX_MISO_SCL|CHANNEL_9|SCI_ODD_CFG|LAST_ITEM_GUARD
+};
+#define P408 P408_b
+
+std::array<uint16_t, 3> getPinCfgs(const pin_size_t pin, PinCfgReq_t req) {
+
+  std::array<uint16_t, 3> ret = {0 , 0, 0};
+  if (pin > g_pin_cfg_size) {
+    return ret;
+  }
+
+  uint8_t cfg_idx = 0;
+  const uint16_t* cfg = g_pin_cfg[pin].list;
+
+  bool thats_all = false;
+  uint8_t index = 0;
+
+  while(!thats_all) {
+
+    if(PIN_CFG_REQ_UART_TX == req && IS_PIN_UART_TX(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_UART_RX == req && IS_PIN_UART_RX(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_SCL == req && IS_PIN_SCL(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_SDA == req && IS_PIN_SDA(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_MISO == req && IS_PIN_MISO(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_MOSI == req && IS_PIN_MOSI(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_SCK == req && IS_PIN_SCK(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_PWM == req && IS_PIN_PWM(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_INTERRUPT == req && IS_PIN_INTERRUPT(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_ADC == req && IS_PIN_ANALOG(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_CAN_RX == req && IS_PIN_CAN_RX(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_CAN_TX == req && IS_PIN_CAN_TX(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+
+    if(IS_LAST_ITEM(*(cfg + index))) {
+      thats_all = true;
+    }
+    else {
+      index++;
+    }
+  }
+  return ret;
+}
+
+extern "C" const PinMuxCfg_t g_pin_cfg[] = { 
+  { BSP_IO_PORT_03_PIN_01,    P301   }, /* (0) D0  -------------------------  DIGITAL  */
+  { BSP_IO_PORT_03_PIN_02,    P302   }, /* (1) D1  */
+  { BSP_IO_PORT_01_PIN_04,    P104   }, /* (2) D2  */
+  { BSP_IO_PORT_01_PIN_05,    P105   }, /* (3) D3~ */
+  { BSP_IO_PORT_01_PIN_06,    P106   }, /* (4) D4  */
+  { BSP_IO_PORT_01_PIN_07,    P107   }, /* (5) D5~ */
+  { BSP_IO_PORT_01_PIN_11,    P111   }, /* (6) D6~ */
+  { BSP_IO_PORT_01_PIN_12,    P112   }, /* (7) D7  */
+  { BSP_IO_PORT_03_PIN_04,    P304   }, /* (8) D8  */
+  { BSP_IO_PORT_03_PIN_03,    P303   }, /* (9) D9~  */
+  { BSP_IO_PORT_01_PIN_03,    P103   }, /* (10) D10~ */
+  { BSP_IO_PORT_04_PIN_11,    P411   }, /* (11) D11~ */
+  { BSP_IO_PORT_04_PIN_10,    P410   }, /* (12) D12 */
+  { BSP_IO_PORT_01_PIN_02,    P102   }, /* (13) D13 */
+  { BSP_IO_PORT_01_PIN_01,    P101   }, /* (14) D14 */
+  { BSP_IO_PORT_01_PIN_00,    P100   }, /* (15) D15 */
+  { BSP_IO_PORT_00_PIN_14,    P014   }, /* (16) A0  --------------------------  ANALOG  */
+  { BSP_IO_PORT_00_PIN_00,    P000   }, /* (17) A1  */
+  { BSP_IO_PORT_00_PIN_01,    P001   }, /* (18) A2  */
+  { BSP_IO_PORT_00_PIN_02,    P002   }, /* (19) A3  */
+  { BSP_IO_PORT_01_PIN_01,    P101   }, /* (20) A4  */
+  { BSP_IO_PORT_01_PIN_00,    P100   }, /* (21) A5  */
+
+  { BSP_IO_PORT_01_PIN_09,    P109   }, /* (22) D22 ------------------------  TX */
+  { BSP_IO_PORT_01_PIN_10,    P110   }, /* (23) D23 ------------------------  RX */
+  { BSP_IO_PORT_05_PIN_01,    P501   }, /* (24) D24 ------------------------- TX WIFI */
+  { BSP_IO_PORT_05_PIN_02,    P502   }, /* (25) D25 ------------------------- RX WIFI */
+
+  { BSP_IO_PORT_04_PIN_00,    P400   }, /* (26) D26  QWIC SCL */
+  { BSP_IO_PORT_04_PIN_01,    P401   }, /* (27) D27  QWIC SDA */
+
+  { BSP_IO_PORT_00_PIN_11,    P011   }, /* (28) D28  */
+  { BSP_IO_PORT_00_PIN_12,    P012   }, /* (29) D29  */
+  { BSP_IO_PORT_00_PIN_13,    P013   }, /* (30) D30  */
+  { BSP_IO_PORT_02_PIN_04,    P204   }, /* (31) D31  */
+  { BSP_IO_PORT_02_PIN_05,    P205   }, /* (32) D32  */
+  { BSP_IO_PORT_02_PIN_06,    P206   }, /* (33) D33  */
+  { BSP_IO_PORT_02_PIN_13,    P213   }, /* (34) D34  */
+  { BSP_IO_PORT_02_PIN_12,    P212   }, /* (35) D35  */
+};
+
+extern "C" const size_t g_pin_cfg_size = sizeof(g_pin_cfg);
+
+int32_t getPinIndex(bsp_io_port_pin_t p) {
+  int max_index = g_pin_cfg_size / sizeof(g_pin_cfg[0]);
+  int rv = -1;
+  for(int i = 0; i < max_index; i++) {
+    if(g_pin_cfg[i].pin == p) {
+      rv = i;
+      break;
+    }
+  }
+  return rv;
+}
+
+#include "FspTimer.h"
+
+void initVariant() {
+  // bootloader configures LED_BUILTIN as PWM output, deconfigure it to avoid spurious signals
+  pinMode(LED_BUILTIN, OUTPUT);
+  //pinMode(LEDB, OUTPUT);
+  //pinMode(LEDR, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  //digitalWrite(LEDB, LOW);
+  //digitalWrite(LEDR, LOW);
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(3, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(5, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(6, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(9, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(10, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(11, PIN_CFG_REQ_PWM)[0]));
+}
