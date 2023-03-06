f343a619da6a73eef2b0d1e05b0425eeea08df38
Alexander Entinger <consulting@lxrobotics.com>
2023-03-06 13:56:03 +0100

    Rename library to Arduino_CAN. This allows consistency with the related ArduinoCore-mbed PR:
    https://github.com/arduino/ArduinoCore-mbed/pull/633.
    
    Former-commit-id: fe982e925fdff454c764c5b6e9f4c95807602f5e

--- libraries/Arduino_CAN/examples/CANRead/CANRead.ino
+++ libraries/Arduino_CAN/examples/CANRead/CANRead.ino
@@ -2,7 +2,7 @@
  * INCLUDE
  **************************************************************************************/
 
-#include <CAN.h>
+#include <Arduino_CAN.h>
 
 /**************************************************************************************
  * SETUP/LOOP
@@ -13,18 +13,7 @@ void setup()
   Serial.begin(115200);
   while (!Serial) { }
 
-  /* You need to enable the CAN transceiver
- * by commenting in below code when using
- * a Portenta H33 on a Portenta Max Carrier.
- * Note: Only CAN1 is available on the Portenta
- * Max Carrier's RJ10 CAN connector.
- */
-#if (PIN_CAN1_STBY >= 0)
-  pinMode(PIN_CAN1_STBY, OUTPUT);
-  digitalWrite(PIN_CAN1_STBY, LOW);
-#endif
-
-  if (!CAN1.begin(CanBitRate::BR_250k))
+  if (!CAN.begin(CanBitRate::BR_250k))
   {
     Serial.println("CAN.begin(...) failed.");
     for (;;) {}
@@ -33,9 +22,9 @@ void setup()
 
 void loop()
 {
-  if (CAN1.available())
+  if (CAN.available())
   {
-    CanMsg const msg = CAN1.read();
+    CanMsg const msg = CAN.read();
     Serial.println(msg);
   }
 }
