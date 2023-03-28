be760cfbbe63c1017219cb87eb3702364bb8c935
Martino Facchin <m.facchin@arduino.cc>
2023-03-28 18:30:34 +0200

    Make getPinCfgs generally available
    Former-commit-id: 692bdbc01f9c857fe4dabad173618f095e428bfd

---
+++ cores/arduino/variant_helper.cpp
@@ -0,0 +1,70 @@
+#include "Arduino.h"
+#include "variant.h"
+
+extern const PinMuxCfg_t g_pin_cfg[];
+extern const size_t g_pin_cfg_size;
+
+std::array<uint16_t, 3> getPinCfgs(const pin_size_t pin, PinCfgReq_t req) {
+
+  std::array<uint16_t, 3> ret = {0 , 0, 0};
+  if (pin > g_pin_cfg_size) {
+    return ret;
+  }
+
+  uint8_t cfg_idx = 0;
+  const uint16_t* cfg = g_pin_cfg[pin].list;
+
+  bool thats_all = false;
+  uint8_t index = 0;
+
+  while(!thats_all) {
+
+    if(PIN_CFG_REQ_UART_TX == req && IS_PIN_UART_TX(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_UART_RX == req && IS_PIN_UART_RX(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_SCL == req && IS_PIN_SCL(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_SDA == req && IS_PIN_SDA(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_MISO == req && IS_PIN_MISO(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_MOSI == req && IS_PIN_MOSI(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_SCK == req && IS_PIN_SCK(*(cfg + index))) {
+        ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_PWM == req && IS_PIN_PWM(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_INTERRUPT == req && IS_PIN_INTERRUPT(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_ADC == req && IS_PIN_ANALOG(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_CAN_RX == req && IS_PIN_CAN_RX(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_CAN_TX == req && IS_PIN_CAN_TX(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+    else if(PIN_CFG_REQ_DAC == req && IS_PIN_DAC(*(cfg + index))) {
+      ret[cfg_idx++] = *(cfg + index);
+    }
+
+    if(IS_LAST_ITEM(*(cfg + index))) {
+      thats_all = true;
+    }
+    else {
+      index++;
+    }
+  }
+  return ret;
+}
--- variants/SANTIAGO/variant.cpp
+++ variants/SANTIAGO/variant.cpp
@@ -17,68 +17,6 @@ SCI_CHANNEL|PIN_RX_MISO_SCL|CHANNEL_9|SCI_ODD_CFG|LAST_ITEM_GUARD
 };
 #define P408 P408_b
 
-std::array<uint16_t, 3> getPinCfgs(const pin_size_t pin, PinCfgReq_t req) {
-
-  std::array<uint16_t, 3> ret = {0 , 0, 0};
-  if (pin > g_pin_cfg_size) {
-    return ret;
-  }
-
-  uint8_t cfg_idx = 0;
-  const uint16_t* cfg = g_pin_cfg[pin].list;
-
-  bool thats_all = false;
-  uint8_t index = 0;
-
-  while(!thats_all) {
-
-    if(PIN_CFG_REQ_UART_TX == req && IS_PIN_UART_TX(*(cfg + index))) {
-      ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_UART_RX == req && IS_PIN_UART_RX(*(cfg + index))) {
-      ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_SCL == req && IS_PIN_SCL(*(cfg + index))) {
-        ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_SDA == req && IS_PIN_SDA(*(cfg + index))) {
-        ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_MISO == req && IS_PIN_MISO(*(cfg + index))) {
-        ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_MOSI == req && IS_PIN_MOSI(*(cfg + index))) {
-        ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_SCK == req && IS_PIN_SCK(*(cfg + index))) {
-        ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_PWM == req && IS_PIN_PWM(*(cfg + index))) {
-      ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_INTERRUPT == req && IS_PIN_INTERRUPT(*(cfg + index))) {
-      ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_ADC == req && IS_PIN_ANALOG(*(cfg + index))) {
-      ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_CAN_RX == req && IS_PIN_CAN_RX(*(cfg + index))) {
-      ret[cfg_idx++] = *(cfg + index);
-    }
-    else if(PIN_CFG_REQ_CAN_TX == req && IS_PIN_CAN_TX(*(cfg + index))) {
-      ret[cfg_idx++] = *(cfg + index);
-    }
-
-    if(IS_LAST_ITEM(*(cfg + index))) {
-      thats_all = true;
-    }
-    else {
-      index++;
-    }
-  }
-  return ret;
-}
-
 extern "C" const PinMuxCfg_t g_pin_cfg[] = { 
   { BSP_IO_PORT_03_PIN_01,    P301   }, /* (0) D0  -------------------------  DIGITAL  */
   { BSP_IO_PORT_03_PIN_02,    P302   }, /* (1) D1  */
