41b14eb3cdf513bbbe0cdd096b918c97ce1362d5
Alexander Entinger <consulting@lxrobotics.com>
2023-04-27 10:58:15 +0200

    Fix: also execute loop function after vTaskStartScheduler.
    Former-commit-id: 19093fba48d8db52011cbcd08ed3de4848b12205

--- libraries/Arduino_FreeRTOS/examples/FreeRTOS-Blink/FreeRTOS-Blink.ino
+++ libraries/Arduino_FreeRTOS/examples/FreeRTOS-Blink/FreeRTOS-Blink.ino
@@ -8,7 +8,7 @@
  * GLOBAL VARIABLES
  **************************************************************************************/
 
-TaskHandle_t blinky_thread_hdl;
+TaskHandle_t loop_task, blinky_task;
 
 /**************************************************************************************
  * SETUP/LOOP
@@ -19,37 +19,78 @@ void setup()
   Serial.begin(115200);
   while (!Serial) { }
 
-  /* Init RTOS tasks. */
-  auto const rc = xTaskCreate
+  pinMode(LEDR, OUTPUT);
+  digitalWrite(LEDR, LOW);
+
+  /* Init a task that calls 'loop'
+   * since after the call to
+   * 'vTaskStartScheduler' we'll never
+   * get out of setup() and therefore
+   * would never get to loop(), as we
+   * are leaving the default execution
+   * flow.
+   */
+#if 1
+  auto const rc_loop = xTaskCreate
+    (
+      loop_thread_func,
+      static_cast<const char*>("Loop Thread"),
+      512 / 4,   /* usStackDepth in words */
+      nullptr,   /* pvParameters */
+      1,         /* uxPriority */
+      &loop_task /* pxCreatedTask */
+    );
+
+  if (rc_loop != pdPASS) {
+    Serial.println("Failed to create 'loop' thread");
+    return;
+  }
+#endif
+#if 1
+  auto const rc_blinky = xTaskCreate
     (
       blinky_thread_func,
       static_cast<const char*>("Blinky Thread"),
-      512 / 4, /* usStackDepth in words */
-      nullptr, /* pvParameters */
-      1, /* uxPriority */
-      &blinky_thread_hdl /* pxCreatedTask */
+      512 / 4,     /* usStackDepth in words */
+      nullptr,     /* pvParameters */
+      1,           /* uxPriority */
+      &blinky_task /* pxCreatedTask */
     );
 
-  if (rc != pdPASS)
+  if (rc_blinky != pdPASS) {
+    Serial.println("Failed to create 'loop' thread");
     return;
-
+  }
+#endif
   Serial.println("Starting scheduler ...");
   /* Start the scheduler. */
-  vTaskStartScheduler ();
+  vTaskStartScheduler();
+  /* We'll never get here. */
+  for( ;; );
 }
 
 void loop()
 {
+  digitalWrite(LEDR, !digitalRead(LEDR));
+  vTaskDelay(configTICK_RATE_HZ/4);
+}
 
+void loop_thread_func(void *pvParameters)
+{
+  for(;;)
+  {
+    loop();
+    taskYIELD();
+  }
 }
 
 void blinky_thread_func(void *pvParameters)
 {
-  /* blinky_thread.setup() */
+  /* setup() */
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
-  /* blinky_thread.loop() */
+  /* loop() */
   for(;;)
   {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
