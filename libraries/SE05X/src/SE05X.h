d9cf39a1e9f35176b3f281b56ef325d1ded91774
pennam <m.pennasilico@arduino.cc>
2023-05-04 12:26:46 +0200

    SE05X: Add getSession()
    Session is needed by mbedtls SE05X alt functions
    
    
    Former-commit-id: b31bb334a3b1ee9191ac09d87b77988875c2f91e

--- libraries/SE05X/src/SE05X.h
+++ libraries/SE05X/src/SE05X.h
@@ -324,6 +324,7 @@ public:
     inline int locked() { return 1; }
     inline int lock() { return 1; }
     inline int writeConfiguration(const byte data[]) { return 1; }
+    inline Se05xSession_t* getSession() { return &_se05x_session; }
 
 private:
     static int getECKeyXyValuesFromDER(byte* derKey, size_t derLen, byte* rawKey, size_t* rawLen);
