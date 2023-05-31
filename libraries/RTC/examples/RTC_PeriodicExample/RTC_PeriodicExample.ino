5cd7f3690adb32e7449372d676a0889279d1340e
alrvid <126816223+alrvid@users.noreply.github.com>
2023-05-31 15:33:09 +0200

    Create RTC_PeriodicExample.ino
    This example demonstrates how to use the periodic callback functionality of the RTC (Real Time Clock) on the Portenta C33.
    
    Former-commit-id: 8ee8b6217723673c773e1067872eac147fcfa6c6

---
+++ libraries/RTC/examples/RTC_PeriodicExample/RTC_PeriodicExample.ino
@@ -0,0 +1,64 @@
+/*
+ * This example demonstrates how to use the periodic callback functionality of the RTC
+ * (Real Time Clock) on the Portenta C33.
+ *
+ * It blinks the built-in LED at progressively faster and slower rates repeatedly.
+ *
+ */
+
+#include "RTC.h"
+
+#define BLINK_DELAY 2000
+
+// This is the callback function to be passed to RTC.setPeriodicCallback()
+void periodicCallback()
+{
+  static bool ledState = false;
+  if (ledState == true) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  ledState = !ledState;
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600);
+  while (!Serial) ;
+    
+  // Initialize the RTC
+  RTC.begin();
+
+  // RTC.setTime() must be called for RTC.setPeriodicCallback to work, but it doesn't matter
+  // what date and time it's set to
+  RTCTime mytime(25, Month::AUGUST, 2022, 14, 37, 00, DayOfWeek::THURSDAY, SaveLight::SAVING_TIME_ACTIVE);
+  RTC.setTime(mytime);
+}
+
+void loop() {
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::ONCE_EVERY_1_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::N2_TIMES_EVERY_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::N4_TIMES_EVERY_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::N8_TIMES_EVERY_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::N16_TIMES_EVERY_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::N32_TIMES_EVERY_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::N16_TIMES_EVERY_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::N8_TIMES_EVERY_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::N4_TIMES_EVERY_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::N2_TIMES_EVERY_SEC));
+  delay(BLINK_DELAY);
+  Serial.println(RTC.setPeriodicCallback(periodicCallback, Period::ONCE_EVERY_1_SEC));
+  delay(BLINK_DELAY);
+}
