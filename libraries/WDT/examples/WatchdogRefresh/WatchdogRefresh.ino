e18075b579e226abcde21f42316d7bf23900f41e
pennam <m.pennasilico@arduino.cc>
2023-06-16 11:12:55 +0200

    Watchdog timer: update example

--- libraries/WDT/examples/WatchdogRefresh/WatchdogRefresh.ino
+++ libraries/WDT/examples/WatchdogRefresh/WatchdogRefresh.ino
@@ -4,31 +4,63 @@
   This sketch shows how to enable the watchdog and
   refresh the timer to avoid resets
 
+  Watchdog intervals are limited to 7 timeout periods
+  the library will select the best clock divisor and timeout
+  according to the selected wdtInterval.
+
+  UNO R4 min wdtInterval 1ms / max wdtInterval 5592ms
+  Comment out Serial.print() in the setup to make it work with
+  small intervals
+
+  Portenta C33 min wdtInterval 1ms / max wdtInterval 2684ms
+
   Circuit:
    - Portenta C33
+   - UNO R4
 */
 
 #include <WDT.h>
+
+const long ledInterval = 1000;
+unsigned long ledMillis = 0;
+bool ledState = true;
+const long wdtInterval = 2684;
+unsigned long wdtMillis = 0;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  wdt_cfg_t p_cfg; 
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  p_cfg.timeout = WDT_TIMEOUT_16384;
-  p_cfg.clock_division = WDT_CLOCK_DIVISION_8192;
-  p_cfg.window_start = WDT_WINDOW_START_100;
-  p_cfg.window_end = WDT_WINDOW_END_0;
-  p_cfg.reset_control = WDT_RESET_CONTROL_RESET;
-  p_cfg.stop_control = WDT_STOP_CONTROL_ENABLE;
+  if(wdtInterval < 1) {
+    Serial.println("Invalid watchdog interval");
+    while(1){}
+  }
 
-  WDT.begin(p_cfg);
+  if(WDT.begin(wdtInterval)) {
+    Serial.print("WDT interval: ");
+    WDT.refresh();
+    Serial.print(WDT.getTimeout());
+    WDT.refresh();
+    Serial.println(" ms");
+    WDT.refresh();
+  } else {
+    Serial.println("Error initializing watchdog");
+    while(1){}
+  }
 }
 
 void loop() {
-  Serial.println("Still Alive...");
-  // Comment the line above to stop refreshing the watchdog
-  WDT.refresh();
-  delay(1000);
+  if(millis() - ledMillis >= ledInterval) {
+    digitalWrite(LED_BUILTIN, ledState);
+    ledState = !ledState;
+    ledMillis = millis();
+  }
+
+  if(millis() - wdtMillis >= wdtInterval - 1) {
+    WDT.refresh(); // Comment this line to stop refreshing the watchdog
+    wdtMillis = millis();
+  }
+
 }
