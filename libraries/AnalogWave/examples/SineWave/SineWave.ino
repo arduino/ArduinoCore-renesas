3bf3c4a1565e16df8c33d5ca5adcf11dd50d5a0d
Jacob Hylén <j.hylen@ext.arduino.cc>
2023-07-18 14:41:31 +0200

    Update sineWave example of AnalogWave

--- libraries/AnalogWave/examples/SineWave/SineWave.ino
+++ libraries/AnalogWave/examples/SineWave/SineWave.ino
@@ -6,12 +6,11 @@ int freq = 10;  // in hertz, change accordingly
 
 void setup() {
   Serial.begin(115200);
-  pinMode(A0, INPUT);
   wave.sine(freq);
 }
 
 void loop() {
-  freq = map(analogRead(A0), 0, 1024, 0, 10000);
+  freq = map(analogRead(A5), 0, 1024, 0, 10000);
   Serial.println("Frequency is now " + String(freq) + " hz");
   wave.freq(freq);
   delay(1000);
