7bef4fcc241ec5d518ccf3dde49eca813a201c51
Kees-van-der-Oord <Kees.van.der.Oord@inter.nl.net>
2023-07-24 21:05:15 +0200

    added const qualifiers, gave register access macro function format, broke down long formulas

--- cores/arduino/time.cpp
+++ cores/arduino/time.cpp
@@ -20,16 +20,16 @@ __attribute__((weak)) void yield() {
 
 static FspTimer main_timer;
 // specifying these details as constants makes micros() faster !
-#define _timer_type           AGT_TIMER
-#define _timer_index          0
-#define _timer_underflow_bit  R_AGT0->AGTCR_b.TUNDF
-#define _timer_clock_divider  TIMER_SOURCE_DIV_8 // dividers 1, 2, 4 and 8 work because _timer_period is < 16-bit. bug in R4 1.0.2: divider 4 acts as 1 !? 
-#define _timer_clock_freq     24000000UL
-#define _timer_ticks_per_us   (_timer_clock_freq / ((1 << _timer_clock_divider) * 1000000UL))
-#define _timer_period         (_timer_ticks_per_us * 1000UL)
-#define TIMER_PRIORITY        8
+#define _timer_type                AGT_TIMER
+#define _timer_index               0
+#define _timer_get_underflow_bit() R_AGT0->AGTCR_b.TUNDF
+#define _timer_clock_divider       TIMER_SOURCE_DIV_8 // dividers 1, 2 and 8 work because _timer_period is < 16-bit. the divider 4 seems not supported: acts as 1
+#define _timer_clock_freq          24000000UL
+#define _timer_counts_per_us       (_timer_clock_freq / ((1 << _timer_clock_divider) * 1000000UL))
+#define _timer_period              (_timer_counts_per_us * 1000UL)
+#define TIMER_PRIORITY             8
 
-static void timer_micros_callback(timer_callback_args_t __attribute((unused)) *p_args) {
+static void timer_micros_callback(timer_callback_args_t __attribute((unused))* p_args) {
 	agt_time_ms += 1;
 }
 
@@ -53,8 +53,13 @@ unsigned long micros() {
 	// Return time in us
 	NVIC_DisableIRQ(main_timer.get_cfg()->cycle_end_irq);
 	uint32_t ms = agt_time_ms;
-	uint32_t cnt = main_timer.get_counter();
-	if (_timer_underflow_bit && (cnt > (_timer_period / 2))) ++ms; // the counter wrapped arround just before it was read
+	uint32_t const down_counts = main_timer.get_counter();
+	if (_timer_get_underflow_bit() && (down_counts > (_timer_period / 2)))
+	{
+		// the counter wrapped arround just before it was read
+		++ms;
+	}
 	NVIC_EnableIRQ(main_timer.get_cfg()->cycle_end_irq);
-	return  ms * 1000 + (((_timer_period - 1) - cnt) / _timer_ticks_per_us);
+	uint32_t const up_counts = (_timer_period - 1) - down_counts;
+	return  (ms * 1000) + (up_counts / _timer_counts_per_us);
 }
