12313450248d3184a6c33dd84467d85f4ab6c563
KurtE <kurte@rockisland.com>
2023-09-15 01:11:17 -0700

    Add method to allow the GPT Timer period not to be buffered. (#131)
    * Add method to allow the GPT Timer period not to be buffered
    
    During code review of changes I made to the Servo library:
    https://github.com/arduino-libraries/Servo/pull/116
    
    @iabdalkader requested that I move all of the platform specific timer changes into the core instead of being in the hardware specific sub-directory of the Servo library.
    
    This change adds a method to tell the GPT timer, that when I make a change to the period (set_period(p) ) that I want the change to happen now and not be buffered.
    
    And updated the set_period method to check for this and directly set the not buffered register with the new period.
    
    Co-authored-by: Ibrahim Abdelkader <i.abdalkader@gmail.com>

--- cores/arduino/FspTimer.cpp
+++ cores/arduino/FspTimer.cpp
@@ -11,7 +11,7 @@ bool FspTimer::force_pwm_reserved = false;
 TimerAvail_t FspTimer::gpt_used_channel[GPT_HOWMANY] = { TIMER_FREE };
 TimerAvail_t FspTimer::agt_used_channel[AGT_HOWMANY] = { TIMER_FREE };
 
-FspTimer::FspTimer(): init_ok(false), agt_timer(nullptr), gpt_timer(nullptr), type(GPT_TIMER) {
+FspTimer::FspTimer(): init_ok(false), agt_timer(nullptr), gpt_timer(nullptr), type(GPT_TIMER), _period_buffer(true) {
     // AGT0 is always used for timekeeping (millis() and micros())
     // agt_used_channel[0] = TIMER_USED;
     timer_cfg.cycle_end_irq = FSP_INVALID_VECTOR;
@@ -453,14 +453,51 @@ bool FspTimer::set_period(uint32_t p) {
 /* -------------------------------------------------------------------------- */    
 
     if(type == GPT_TIMER && gpt_timer != nullptr) {
+        if (_period_buffer) {
         if (R_GPT_PeriodSet(&(gpt_timer->ctrl), p) != FSP_SUCCESS) {
             return false;
+        }
+    }
+        else {
+            // Not buffered set it directly
+            gpt_timer->ctrl.p_reg->GTPR = p;
         }
     }
     else if(type == AGT_TIMER && agt_timer != nullptr) {
         if (R_AGT_PeriodSet(&(agt_timer->ctrl), p) != FSP_SUCCESS) {
             return false;
         }
+    }
+    else {
+        return false;
+    }
+    return true;
+}
+
+
+
+/* -------------------------------------------------------------------------- */
+bool FspTimer::set_period_buffer(bool period_buffer) {
+/* -------------------------------------------------------------------------- */    
+
+    if (_period_buffer == (uint8_t)period_buffer) {
+        return true;
+    }
+
+    _period_buffer = (uint8_t)period_buffer;
+    if(type == GPT_TIMER && gpt_timer != nullptr) {
+
+        if (period_buffer) {
+            gpt_timer->ctrl.p_reg->GTBER_b.PR = 1;
+            gpt_timer->ctrl.p_reg->GTBER_b.BD1 = 0;
+        }
+        else {
+            gpt_timer->ctrl.p_reg->GTBER_b.PR = 0;
+            gpt_timer->ctrl.p_reg->GTBER_b.BD1 = 1;
+        } 
+    }
+    else if(type == AGT_TIMER && agt_timer != nullptr) {
+        // not buffered..
     }
     else {
         return false;
