378a3ce34cdfcaffb9892065e60d130aed8dbd30
Woodwarth <holzwarthm@yahoo.de>
2023-11-03 10:06:16 +0100

    Update CANReadFilter.ino
    Updated Description

--- libraries/Arduino_CAN/examples/CANReadFilter/CANReadFilter.ino
+++ libraries/Arduino_CAN/examples/CANReadFilter/CANReadFilter.ino
@@ -2,11 +2,16 @@
   CANRead
 
   Receive and read CAN Bus messages
+  Read ID 0x100 EXTENDED and ID 0x10 STANDARD
   
   
   Mailbox Groups 0-3 (Mailbox  0-15) -> TRANSMIT
   Mailbox Groups 4-5 (Mailbox 16-23) -> RECEIVE EXTENDED
   Mailbox Groups 6-7 (Mailbox 24-31) -> RECEIVE STANDARD
+
+  Mailbox Mask 5,7 are preset with 0x1FFFFFFF
+  Mailbox Mask 4,6 are preset with 0x00
+
 
   See the full documentation here:
   https://docs.arduino.cc/tutorials/uno-r4-wifi/can
@@ -18,6 +23,9 @@
 
 #include <Arduino_CAN.h>
 
+#define Mask11Bit 0x1FFC0000 
+#define Mask29Bit 0x1FFFFFFF 
+
 /**************************************************************************************
  * SETUP/LOOP
  **************************************************************************************/
@@ -27,10 +35,14 @@ void setup()
   Serial.begin(115200);
   while (!Serial) { }
 
-  CAN.setMailboxMask(4, 0x1FFFFFFF);
-  CAN.setMailboxMask(6, 0x1FFFFFFF);
+  CAN.setMailboxMask(4, Mask29Bit);
+  CAN.setMailboxMask(6, Mask11Bit);
 
-  for (int c=16; c <= 31; c++) {
+  for (int c=16; c <= 23; c++) {
+    CAN.setMailboxID(c, 0x0100);
+  }
+
+  for (int c=24; c <= 31; c++) {
     CAN.setMailboxID(c, 0x10);
   }
 
