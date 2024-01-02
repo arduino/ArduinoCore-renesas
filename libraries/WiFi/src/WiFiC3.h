f911f6711ae3e1f931613128f8fe8c89bb23a1fb
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-01-02 16:08:43 +0100

    defined Wifi global variable for network interface

--- libraries/WiFi/src/WiFiC3.h
+++ libraries/WiFi/src/WiFiC3.h
@@ -8,7 +8,11 @@
 
 #define WIFI_FIRMWARE_LATEST_VERSION "1.5.0"
 
+#ifdef ARDUINO_PORTENTA_C33
 // TODO Instantiate the drivers for wifi with default configuration parameters
+// ESPHostFGDriver WifiDriver;
 
 // Instantiate a global variable from CWifiStation calling it WiFi
 
+inline CWifiStation WiFi;
+#endif // ARDUINO_PORTENTA_C33
