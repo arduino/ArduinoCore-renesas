144688e523b9e26c14668d57a82ca706cf3620dd
iabdalkader <i.abdalkader@gmail.com>
2024-05-13 13:49:08 +0200

    libraries/SoftwareSerial: Fix ring buffer data type.
    Fixes #243.
    
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- libraries/SoftwareSerial/src/SoftwareSerial.h
+++ libraries/SoftwareSerial/src/SoftwareSerial.h
@@ -57,7 +57,7 @@ template <size_t N> struct ss_descr_t {
     fsp_dma_t dma;
     fsp_pin_t pin;
     int irq_chan;
-    ::RingBuffer<char> ringbuf;
+    ::RingBuffer<uint8_t> ringbuf;
     uint32_t dmabuf[N][SS_MAX_FRAME_SIZE] __attribute__((aligned(4)));
     ss_descr_t(size_t bufsize): irq_chan(-1), ringbuf(bufsize) {
     }
