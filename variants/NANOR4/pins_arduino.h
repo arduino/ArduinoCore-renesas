5cfd8609a9e2e711ce91d082f9f66e73b7092655
Martino Facchin <m.facchin@arduino.cc>
2025-01-21 15:33:06 +0100

    boards: add Nano R4
    Shares many features with Minima, so most of the support code is copied (not regenerated)

---
+++ variants/NANOR4/pins_arduino.h
@@ -0,0 +1,192 @@
+#pragma once
+
+#define PIN(X,Y) (X * 16 + Y)
+
+// Pin count
+// ----
+#ifdef __cplusplus
+extern "C" unsigned int PINCOUNT_fn();
+#endif
+#define PINS_COUNT           (PINCOUNT_fn())
+#define NUM_DIGITAL_PINS     (20u)
+#define NUM_ANALOG_INPUTS    (8u)
+#define NUM_ANALOG_OUTPUTS   (1u)
+
+#define PIN_A0   (14u)
+#define PIN_A1   (15u)
+#define PIN_A2   (16u)
+#define PIN_A3   (17u)
+#define PIN_A4   (18u)
+#define PIN_A5   (19u)
+#define PIN_A6   (20u)
+#define PIN_A7   (21u)
+
+#define DAC8_HOWMANY (0)
+#define DAC12_HOWMANY (1)
+#define DAC         A0
+#define IS_DAC(x)   (x == DAC ? true : false)
+#define DAC_ADDRESS_12_CH0          R_DAC_BASE
+
+#undef A0
+#undef A1
+#undef A2
+#undef A3
+#undef A4
+#undef A5
+#undef A6
+#undef A7
+static const uint8_t A0 = PIN_A0;
+static const uint8_t A1 = PIN_A1;
+static const uint8_t A2 = PIN_A2;
+static const uint8_t A3 = PIN_A3;
+static const uint8_t A4 = PIN_A4;
+static const uint8_t A5 = PIN_A5;
+static const uint8_t A6 = PIN_A6;
+static const uint8_t A7 = PIN_A7;
+
+// Digital pins
+// -----------
+#define PIN_D0   (0u)
+#define PIN_D1   (1u)
+#define PIN_D2   (2u)
+#define PIN_D3   (3u)
+#define PIN_D4   (4u)
+#define PIN_D5   (5u)
+#define PIN_D6   (6u)
+#define PIN_D7   (7u)
+#define PIN_D8   (8u)
+#define PIN_D9   (9u)
+#define PIN_D10   (10u)
+#define PIN_D11   (11u)
+#define PIN_D12   (12u)
+#define PIN_D13   (13u)
+#define PIN_D14   (14u)
+#define PIN_D15   (15u)
+
+#undef D0
+#undef D1
+#undef D2
+#undef D3
+#undef D4
+#undef D5
+#undef D6
+#undef D7
+#undef D8
+#undef D9
+#undef D10
+#undef D11
+#undef D12
+#undef D13
+#undef D14
+#undef D15
+static const uint8_t D0 = PIN_D0;
+static const uint8_t D1 = PIN_D1;
+static const uint8_t D2 = PIN_D2;
+static const uint8_t D3 = PIN_D3;
+static const uint8_t D4 = PIN_D4;
+static const uint8_t D5 = PIN_D5;
+static const uint8_t D6 = PIN_D6;
+static const uint8_t D7 = PIN_D7;
+static const uint8_t D8 = PIN_D8;
+static const uint8_t D9 = PIN_D9;
+static const uint8_t D10 = PIN_D10;
+static const uint8_t D11 = PIN_D11;
+static const uint8_t D12 = PIN_D12;
+static const uint8_t D13 = PIN_D13;
+static const uint8_t D14 = PIN_D14;
+static const uint8_t D15 = PIN_D15;
+
+#define digitalPinHasPWM(p) (IS_PIN_PWM(getPinCfgs(p, PIN_CFG_REQ_PWM)[0]))
+// LEDs
+// ----
+#define PIN_LED     (22u)
+#define LED_BUILTIN PIN_LED
+#define LEDR        (23u)
+#define LEDG        (24u)
+#define LEDB        (25u)
+
+/****** RTC CORE DEFINES *******/
+#define RTC_HOWMANY     1
+
+
+/****** UART CORE DEFINES ******/
+
+#define SERIAL_HOWMANY		1
+#define UART1_TX_PIN        1
+#define UART1_RX_PIN        0
+
+/****** WIRE CORE DEFINES ******/
+
+#define WIRE_HOWMANY      2
+#define WIRE_SDA_PIN      18 /* A4 */
+#define WIRE_SCL_PIN      19 /* A5 */
+#define WIRE1_SDA_PIN     27
+#define WIRE1_SCL_PIN     26
+
+static const uint8_t SDA = WIRE_SDA_PIN;
+static const uint8_t SCL = WIRE_SCL_PIN;
+
+/****** SPI CORE DEFINES ******/
+
+#define SPI_HOWMANY       1
+
+#define PIN_SPI_MOSI  (11)
+#define PIN_SPI_MISO  (12)
+#define PIN_SPI_SCK   (13)
+#define PIN_SPI_CS    (10)
+#define FORCE_SPI_MODE  (MODE_SPI)
+
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
+static const uint8_t CS  =  PIN_SPI_CS;
+static const uint8_t SS  =  PIN_SPI_CS;
+
+/****** GTP CORE DEFINES *******/
+
+#define GTP32_HOWMANY     2
+#define GTP16_HOWMANY     6
+#define GPT_HOWMANY       8
+
+/****** AGT CORE DEFINES *******/
+#define AGT_HOWMANY       2
+
+
+/****** CAN CORE DEFINES ******/
+
+#define CAN_HOWMANY       1
+
+#define PIN_CAN0_TX       (4)
+#define PIN_CAN0_RX       (5)
+#define PIN_CAN0_STBY    (-1)
+
+
+#define EXT_INTERRUPTS_HOWMANY 2
+
+#define AVCC_MEASURE_PIN        20
+#define AVCC_MULTIPLY_FACTOR    8.33
+
+#define AR_INTERNAL_VOLTAGE     1.43f
+
+#define USB_VID           (0x2341)
+#define USB_PID           (0x0074)
+#define USB_NAME          "Nano R4"
+
+#define VUSB_LDO_ENABLE     1
+
+/* EEPROM DEFINES */
+
+#define ARDUINO_FLASH_TYPE  LP_FLASH
+#define FLASH_BASE_ADDRESS  0x40100000
+#define FLASH_TOTAL_SIZE    0x2000
+#define FLASH_BLOCK_SIZE    0x400
+
+// TODO: removeme
+#ifdef __cplusplus
+extern "C" {
+#endif
+void iic_slave_tei_isr(void);
+void iic_slave_eri_isr(void);
+#ifdef __cplusplus
+}
+#endif
---
+++ variants/NANOR4/variant.cpp
@@ -0,0 +1,101 @@
+#include "Arduino.h"
+#include "pinmux.inc"
+
+const uint16_t P400_b[] = {
+PIN_PWM|CHANNEL_6|PWM_CHANNEL_A|GPT_ODD_CFG,
+PIN_SCL|CHANNEL_0,
+PIN_INTERRUPT|CHANNEL_0,
+SCI_CHANNEL|PIN_SCK|CHANNEL_0|SCI_EVEN_CFG|LAST_ITEM_GUARD
+};
+#define P400 P400_b
+
+const uint16_t P408_b[] = {
+PIN_PWM|CHANNEL_5|PWM_CHANNEL_B|GPT_ODD_CFG,
+PIN_INTERRUPT|CHANNEL_7,
+SCI_CHANNEL|PIN_CTS_RTS_SS|CHANNEL_1|SCI_EVEN_CFG,
+SCI_CHANNEL|PIN_RX_MISO_SCL|CHANNEL_9|SCI_ODD_CFG|LAST_ITEM_GUARD
+};
+#define P408 P408_b
+
+const uint16_t P014_b[] = {
+PIN_DAC|CHANNEL_0,
+PIN_ANALOG|CHANNEL_9|LAST_ITEM_GUARD
+};
+#define P014 P014_b
+
+extern "C" const PinMuxCfg_t g_pin_cfg[] = { 
+  { BSP_IO_PORT_03_PIN_01,    P301   }, /* (0) D0  -------------------------  DIGITAL  */
+  { BSP_IO_PORT_03_PIN_02,    P302   }, /* (1) D1  */
+  { BSP_IO_PORT_01_PIN_05,    P105   }, /* (2) D2  */
+  { BSP_IO_PORT_01_PIN_04,    P104   }, /* (3) D3~ */
+  { BSP_IO_PORT_01_PIN_03,    P103   }, /* (4) D4  */
+  { BSP_IO_PORT_01_PIN_02,    P102   }, /* (5) D5~ */
+  { BSP_IO_PORT_01_PIN_06,    P106   }, /* (6) D6~ */
+  { BSP_IO_PORT_01_PIN_07,    P107   }, /* (7) D7  */
+  { BSP_IO_PORT_03_PIN_04,    P304   }, /* (8) D8  */
+  { BSP_IO_PORT_03_PIN_03,    P303   }, /* (9) D9~  */
+  { BSP_IO_PORT_01_PIN_12,    P112   }, /* (10) D10~ */
+  { BSP_IO_PORT_01_PIN_09,    P109   }, /* (11) D11~ */
+  { BSP_IO_PORT_01_PIN_10,    P110   }, /* (12) D12 */
+  { BSP_IO_PORT_01_PIN_11,    P111   }, /* (13) D13 */
+  { BSP_IO_PORT_00_PIN_14,    P014   }, /* (14) A0  --------------------------  ANALOG  */
+  { BSP_IO_PORT_00_PIN_00,    P000   }, /* (15) A1  */
+  { BSP_IO_PORT_00_PIN_01,    P001   }, /* (16) A2  */
+  { BSP_IO_PORT_00_PIN_02,    P002   }, /* (17) A3  */
+  { BSP_IO_PORT_01_PIN_01,    P101   }, /* (18) A4/SDA  */
+  { BSP_IO_PORT_01_PIN_00,    P100   }, /* (19) A5/SCL  */
+  { BSP_IO_PORT_00_PIN_04,    P004   }, /* (20) A6  */
+  { BSP_IO_PORT_00_PIN_03,    P003   }, /* (21) A7  */
+
+  { BSP_IO_PORT_02_PIN_04,    P204   }, /* (22) LED_BUILTIN  */
+
+  { BSP_IO_PORT_04_PIN_09,    P409   }, /* (23) RED LED  */
+  { BSP_IO_PORT_04_PIN_10,    P410   }, /* (24) GREEN LED  */
+  { BSP_IO_PORT_04_PIN_11,    P411   }, /* (25) BLUE LED  */
+
+  { BSP_IO_PORT_04_PIN_00,    P400   }, /* (26) QWIIK SCL  */
+  { BSP_IO_PORT_04_PIN_01,    P401   }, /* (27) QWIIK SDA  */
+
+};
+
+extern "C" {
+    unsigned int PINCOUNT_fn() {
+        return (sizeof(g_pin_cfg) / sizeof(g_pin_cfg[0]));
+    }
+}
+
+int32_t getPinIndex(bsp_io_port_pin_t p) {
+  int max_index = PINS_COUNT;
+  int rv = -1;
+  for(int i = 0; i < max_index; i++) {
+    if(g_pin_cfg[i].pin == p) {
+      rv = i;
+      break;
+    }
+  }
+  return rv;
+}
+
+#include "FspTimer.h"
+
+void usb_post_initialization() {
+  ((R_USB_FS0_Type*)R_USB_FS0_BASE)->USBMC_b.VDCEN = 1;
+}
+
+void enableSubclockInputPins() {
+  R_BSP_RegisterProtectDisable(BSP_REG_PROTECT_CGC);
+  R_SYSTEM->SOSCCR_b.SOSTP = 1;
+  R_BSP_RegisterProtectEnable(BSP_REG_PROTECT_CGC);
+}
+
+void initVariant() {
+  // bootloader configures LED_BUILTIN as PWM output, deconfigure it to avoid spurious signals
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(3, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(5, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(6, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(9, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(10, PIN_CFG_REQ_PWM)[0]));
+  FspTimer::set_initial_timer_channel_as_pwm(GPT_TIMER, GET_CHANNEL(getPinCfgs(11, PIN_CFG_REQ_PWM)[0]));
+}
