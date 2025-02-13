3581fc2471091e2f928caebbcd784be8bacb8b23
pennam <m.pennasilico@arduino.cc>
2025-02-13 11:13:14 +0100

    WiFiC3: add missing WL*_LENGTH definitions

--- libraries/lwIpWrapper/src/CNetIf.h
+++ libraries/lwIpWrapper/src/CNetIf.h
@@ -34,14 +34,21 @@
 #endif
 
 #define MAX_SOFAT_CONNECTION_DEF 5
-
-#define MAC_ADDRESS_DIM 6
 #define NETWORK_INTERFACES_MAX_NUM 3
 #define MAX_HOSTNAME_DIM 253
 
 #define WIFI_INIT_TIMEOUT_MS 10000
 
+// Maximum size of a SSID
+#define WL_SSID_MAX_LENGTH 32
+// Length of passphrase. Valid lengths are 8-63.
+#define WL_WPA_KEY_MAX_LENGTH 63
+// Length of key in bytes. Valid values are 5 and 13.
+#define WL_WEP_KEY_MAX_LENGTH 13
+// Size of a MAC-address or BSSID
 #define WL_MAC_ADDR_LENGTH 6
+// Size of a MAC-address or BSSID
+#define WL_IPV4_LENGTH 4
 
 /* DEFAULT ADDRESS FOR ETHERNET CONFIGURATION */
 
