b5bb105cb66dea94f9e8971c303a8e8d83ac97b6
Mattia Pennasilico <m.pennasilico@arduino.cc>
2025-02-19 17:38:22 +0100

    lwIpWrapper: fix comment about IP4 address length
    Co-authored-by: Daniele <34984733+maidnl@users.noreply.github.com>

--- libraries/lwIpWrapper/src/CNetIf.h
+++ libraries/lwIpWrapper/src/CNetIf.h
@@ -47,7 +47,7 @@
 #define WL_WEP_KEY_MAX_LENGTH 13
 // Size of a MAC-address or BSSID
 #define WL_MAC_ADDR_LENGTH 6
-// Size of a MAC-address or BSSID
+// Size of a IP4 address
 #define WL_IPV4_LENGTH 4
 
 /* DEFAULT ADDRESS FOR ETHERNET CONFIGURATION */
